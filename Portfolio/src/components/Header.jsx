82cc1934c4469e541eab1083958b7f96f99421ec
Trilok Lowanshi <trilokl01c@gmail.com>
2026-02-06 16:45:40 +0530

    We need to finish this project soon

--- Portfolio/src/components/Header.jsx
+++ Portfolio/src/components/Header.jsx
@@ -1,7 +1,15 @@
 export default function Header() {
     return (
         <div>
-            <p>This is header</p>
+            <div className="header-section">
+                <nav>
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Projects</a>
+                    <a href="#">Blog</a>
+                    <a href="#">Contact</a>
+                </nav>
+            </div>
         </div>
     );
 }
